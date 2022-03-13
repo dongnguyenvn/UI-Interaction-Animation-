83ec5d8daed04d98a1f22710dc15543806c2ebfa
Nguyen Thanh Dong <nguyenthanhdong997@gmail.com>
2022-03-13 14:14:43 +0700

    init

--- react/LandingPageWithScroll/src/App.jsx
+++ react/LandingPageWithScroll/src/App.jsx
@@ -2,12 +2,63 @@ import './App.scss';
 import Image1 from './img/photo-1536179247184-595913ca4a28.jpeg';
 import Image2 from './img/photo-1469817805249-72b7df1c3c05.jpeg';
 import Image3 from './img/photo-1593016605825-8cfb409ca3b1.jpeg';
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+    const headingRef = useRef();
+    const menuRef = useRef();
+    const menuSecondRef = useRef();
+    const imageFirstRef = useRef();
+    const imageSecondRef = useRef();
+    const secondSectionRef = useRef();
+
+    useEffect(() => {
+        gsap.from(menuRef.current.children, {
+            duration: 0.5,
+            opacity: 0,
+            y: -20,
+            stagger: 0.2,
+            delay: 0.5,
+        });
+
+        gsap.to(imageSecondRef.current, {
+            scrollTrigger: {
+                trigger: imageSecondRef.current,
+                scrub: true,
+                start: '80% center',
+                markers: true,
+            },
+            duration: 1.1,
+            scale: 1.2,
+            height: 380,
+        });
+        gsap.to(imageFirstRef.current, {
+            scrollTrigger: {
+                trigger: imageFirstRef.current,
+                scrub: true,
+                start: '110% center',
+            },
+            duration: 1.1,
+            scale: 1.2,
+            height: 250,
+        });
+        gsap.to(headingRef.current, {
+            scrollTrigger: {
+                trigger: headingRef.current,
+                scrub: true,
+                start: '150% center',
+            },
+            color: '#fff',
+            duration: 1.5,
+        });
+    }, []);
     return (
         <div className="main">
             <header className="header">
-                <ul className="menu">
+                <ul className="menu" ref={menuRef}>
                     <li className="menu__item">
                         <a href="#" className="menu__link">
                             Home
@@ -34,42 +85,42 @@ function App() {
                         </a>
                     </li>
                 </ul>
-                <ul className="menu menu-right">
+                <ul className="menu menu-right" ref={menuSecondRef}>
                     <li className="menu__item">
                         <a href="#" className="menu__link">
-                            Home
+                            Blog
                         </a>
                     </li>
                     <li className="menu__item">
                         <a href="#" className="menu__link">
-                            About us
+                            Faq
                         </a>
                     </li>
                 </ul>
             </header>
             <section className="first-section">
                 <div className="info">
-                    <h1 className="heading-1">
+                    <h1 className="heading-1" ref={headingRef}>
                         Real estate advice for real locals.
                     </h1>
                     <p className="paragraph">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Velit voluptatem est quas minus, facilis odit.
                     </p>
-                    <a href="#" className="bnt bnt--start">
+                    <a href="#" className="btn btn--start">
                         Get Started
                     </a>
                 </div>
                 <div className="visuals">
                     <div className="image-container">
-                        <img src={Image1} alt="l" />
+                        <img src={Image1} alt="l" ref={imageFirstRef} />
                     </div>
                     <div className="image-container">
-                        <img src={Image2} alt="l" />
+                        <img src={Image2} alt="l" ref={imageSecondRef} />
                     </div>
                 </div>
             </section>
-            <section className="second-section">
+            <section className="second-section" ref={secondSectionRef}>
                 <div className="box"></div>
                 <h4 className="heading-4" id="about">
                     About Us
